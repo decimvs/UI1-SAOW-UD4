849c0cc313814027a10eed09b926bca4218e4a63
Guillermo Espert <gespert@yahoo.es>
2020-02-16 16:31:44 +0100

    -Añadidos iconos para los enlaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as Feather from 'feather-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ec-ud4';
+export class AppComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    Feather.replace();
+  }
 }
+
